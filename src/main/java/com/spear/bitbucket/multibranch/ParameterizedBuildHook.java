495c99383c042f469c0c3a99502a7f61a3d1d026
George Lezhava <glezhava@hiqinvest.nl>
2016-05-19 14:49:21 +0200

    Ignoring commits done by maven-release-plugin

--- src/main/java/com/spear/bitbucket/multibranch/ParameterizedBuildHook.java
+++ src/main/java/com/spear/bitbucket/multibranch/ParameterizedBuildHook.java
@@ -4,6 +4,9 @@ import java.util.Collection;
 import java.util.regex.Pattern;
 import java.util.regex.PatternSyntaxException;
 
+import com.atlassian.bitbucket.commit.Commit;
+import com.atlassian.bitbucket.commit.CommitService;
+import com.atlassian.bitbucket.commit.CommitsBetweenRequest;
 import com.atlassian.bitbucket.hook.repository.AsyncPostReceiveRepositoryHook;
 import com.atlassian.bitbucket.hook.repository.RepositoryHookContext;
 import com.atlassian.bitbucket.repository.RefChange;
@@ -12,6 +15,8 @@ import com.atlassian.bitbucket.repository.Repository;
 import com.atlassian.bitbucket.setting.RepositorySettingsValidator;
 import com.atlassian.bitbucket.setting.Settings;
 import com.atlassian.bitbucket.setting.SettingsValidationErrors;
+import com.atlassian.bitbucket.util.Page;
+import com.atlassian.bitbucket.util.PageRequestImpl;
 import com.spear.bitbucket.multibranch.ciserver.BuildInfo;
 import com.spear.bitbucket.multibranch.ciserver.GlobalSettings;
 import com.spear.bitbucket.multibranch.ciserver.Jenkins;
@@ -24,27 +29,50 @@ public class ParameterizedBuildHook implements AsyncPostReceiveRepositoryHook, R
 	private static final String REFS_HEADS = "refs/heads/";
 
 	private final SettingsService settingsService;
+	private final CommitService commitService;
 	private final Jenkins jenkins;
+	private static final PageRequestImpl PAGE_REQUEST = new PageRequestImpl(0, 100);
+	private static final String MAVEN_RELEASE_IGNORE_PATTERN = "[maven-release-plugin]";
+	
 
-	public ParameterizedBuildHook(SettingsService settingsService, Jenkins jenkins) {
+	public ParameterizedBuildHook(SettingsService settingsService, CommitService commitService, Jenkins jenkins) {
 		this.settingsService = settingsService;
+		this.commitService = commitService;
 		this.jenkins = jenkins;
 	}
 
 	@Override
 	public void postReceive(RepositoryHookContext context, Collection<RefChange> refChanges) {
+		RepoSettings repoSettings = settingsService.getRepoSettings(context.getSettings());
 		for (RefChange refChange : refChanges) {
 			String branch = refChange.getRef().getId().replace(REFS_HEADS, "");
 			String commit = refChange.getToHash();
-			RepoSettings repoSettings = settingsService.getRepoSettings(context.getSettings());
+			
 			String jenkinsProjectName = repoSettings.getJenkinsProjectName();
+			final CommitsBetweenRequest changesetsBetweenRequest = new CommitsBetweenRequest.Builder(context.getRepository())
+                    .include(refChange.getToHash())
+                    .exclude(refChange.getFromHash())
+                    .build();
 			Trigger trigger = buildBranchCheck(refChange, branch, repoSettings.getBranchRegex());
+			if (refChange.getType() == RefChangeType.UPDATE && !checkCommitMessageValid(commitService.getCommitsBetween(changesetsBetweenRequest, PAGE_REQUEST)))
+				continue;
+
+			
 			BuildInfo buildInfo = new BuildInfo(branch, null, commit, null, "Auto-triggered for branch " + branch, trigger);
 
 			jenkins.triggerJob(buildInfo, jenkinsProjectName);
 		}
 	}
 
+	private boolean checkCommitMessageValid(Page<Commit> commits) {
+		for (Commit changeset : commits.getValues()) {
+			// System.err.println("Commit message : " + changeset.getme);
+			if (changeset.getMessage() != null && changeset.getMessage().startsWith(MAVEN_RELEASE_IGNORE_PATTERN)) {
+				return false;
+			}
+		}
+		return true;
+	}
 	public Trigger buildBranchCheck(RefChange refChange, String branch, String branchCheck) {
 		if (refChange.getType() == RefChangeType.UPDATE) {
 			if ((branchCheck.isEmpty() || branch.toLowerCase().matches(branchCheck.toLowerCase()))) {
